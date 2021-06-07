3ec5a43f6ed0c0bd0f0a18b6ad9597fa96278dcd
hazra1991 <abhishekhazra007@gmail.com>
2021-06-07 22:44:48 +0530

    pincode added,Connection error handeled: __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from entrypoint import getCalender,monitorAvailabitily , getFromPincodeList
 from argparse import ArgumentParser
+from requests.exceptions import ConnectionError
 
 my_args = ArgumentParser(description='Find the cowin slot availability',prog='Cowin Nvigator')
 mygroup = my_args.add_mutually_exclusive_group( required=True)
@@ -14,25 +15,30 @@ my_args.add_argument('--dose',metavar = '',action='store',choices=['1','2'],help
 args = my_args.parse_args()
 user_args = {'min_age':args.age_limit,'from_date':args.date,'dose':args.dose}
 pin=state=district = None
-if args.bypin:
-    pin = input('Please enter the location pin number :- ')
-    user_args.update({'pin':pin})
-elif args.bydistrict:
-    state = input('Please enter the state name:- ')
-    district =input('Please enter the district name:- ')
-    user_args.update({'state':state,'district':district})
-elif args.all_pin_file and not args.monitor:
-    getFromPincodeList(args.all_pin_file,user_args)
-    exit()
-else:
-    print("\n\n[**] Cannot monitor(-m,--monitor) with --all-pin flag else use -p or -bd or dissable th -m param")
-    exit()
+try:
 
-if not args.monitor:
-    getCalender(**user_args)
-    
-else:
-    email = input("Please specify the email address to sent notification to :-- ")
-    print('\t\t\-----Monitoring-------------- ')
-    monitorAvailabitily(email,**user_args)
+    if args.bypin:
+        pin = input('Please enter the location pin number :- ').strip()
+        user_args.update({'pin':pin})
+    elif args.bydistrict:
+        state = input('Please enter the state name:- ').strip()
+        district =input('Please enter the district name:- ').strip()
+        user_args.update({'state':state,'district':district})
+    elif args.all_pin_file and not args.monitor:
+        getFromPincodeList(args.all_pin_file,user_args)
+        exit()
+    else:
+        print("\n\n[**] Cannot monitor(-m,--monitor) with --all-pin flag else use -p or -bd or dissable th -m param")
+        exit()
+
+    if not args.monitor:
+        getCalender(**user_args)
+        
+    else:
+        email = input("Please specify the email address to sent notification to :-- ")
+        print('\t\t\-----Monitoring-------------- ')
+        monitorAvailabitily(email,**user_args)
+except ConnectionError:
+    print('\n\n[[++]] Could not establish connection , Pleas check the internet connection')
+
 
